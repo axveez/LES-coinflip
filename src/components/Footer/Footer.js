69af1eea89df208ae3b8d344d86e7b6af4fa63f4
Tommy <tommymaher10@gmail.com>
2022-03-19 16:53:20 +0000

    refactor footer wip

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDiscord, faTwitter } from "@fortawesome/free-brands-svg-icons";
+
+import parasLogo from '../../assets/img/paras-logo.png';
+
+function Footer() {
+
+
+
+    const icons = [
+        {faIcon: faTwitter, href: 'https://twitter.com/TheDonsProject'},
+        {faIcon: faDiscord, href: 'https://discord.com/invite/thedons'}
+    ];
+
+    const ShowFooter = () => {
+        return (
+        <div className="m-1 mt-5">
+            <div className="row justify-content-center">
+                <ul className="d-flex flex-row justify-content-center social">
+                    <li className="nav-item nav-link">
+                        <a href='https://paras.id'>
+                            <img src={parasLogo} alt='paras' style={{ width: 25 }} />
+                        </a>
+                    </li>
+                    {icons.map((icon, i) => 
+                    <li className="nav-item nav-link" key={i}>
+                        <a href={icon.href}>
+                        <FontAwesomeIcon
+                            icon={icon.faIcon}
+                            style={{ fontSize: 30, color: "black", paddingTop: 10 }}
+                        />
+                        </a>
+                    </li>
+                    )}
+                </ul>  
+            </div>
+
+            <div className='row justify-content-center'><span className='bottom-line'></span></div>
+        </div>
+        )
+    }
+
+
+}
+
+export default Footer;
