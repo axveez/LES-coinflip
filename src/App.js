b3583b7507a9d304e9fd24aaf9f46a4a57eaff63
Tommy <tommymaher10@gmail.com>
2022-03-20 19:20:07 +0000

    refactoring work

--- src/App.js
+++ src/App.js
@@ -1,64 +1,54 @@
 import "regenerator-runtime/runtime";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { login, logout } from "./utils";
 import "./global.css";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import FlipData from "./components/FlipData/FlipData";
 import CoinLogoTails from "./assets/svg/tails-logo.png";
 import CoinLogoHeads from "./assets/svg/heads-logo.png";
-import RealTimeIcon from "./assets/img/realtime.png";
 import getConfig from "./config";
 
-import axios from "axios";
 const { networkId } = getConfig(process.env.NODE_ENV || "development");
 
 export default function App() {
-  // use React Hooks to store greeting in component state
-
-  // when the user has not yet interacted with the form, disable the button
   const [buttonDisabled, setButtonDisabled] = React.useState(true);
-
-  // after submitting the form, we want to show Notification
   const [showNotification, setShowNotification] = React.useState(false);
 
-  // The useEffect hook can be used to fire side-effects during render
-  // Learn more: https://reactjs.org/docs/hooks-intro.html
-  React.useEffect(
-    () => {
-      // in this case, we only care to query the contract when signed in
-      if (window.walletConnection.isSignedIn()) {
-        // window.contract is set by initContract in index.js
-        window.contract
-          .get_greeting({ account_id: window.accountId })
-          .then((greetingFromContract) => {
-            set_greeting(greetingFromContract);
-          });
-      }
-    },
+  React.useEffect(() => {
+    // in this case, we only care to query the contract when signed in
+    if (window.walletConnection.isSignedIn()) {
+      // window.contract is set by initContract in index.js
+      window.contract
+        .get_greeting({ account_id: window.accountId })
+        .then((greetingFromContract) => {
+          set_greeting(greetingFromContract);
+        });
+    }
+  }, []);
 
-    // The second argument to useEffect tells React when to re-run the effect
-    // Use an empty array to specify "only run on first render"
-    // This works because signing into NEAR Wallet reloads the page
-    []
-  );
-
-  // // if not signed in, return early with sign-in prompt
-  // if (!window.walletConnection.isSignedIn()) {
-  //   return (
-  //       <Header />
-
-  //   )
-  // }
-
+  const baseURL =
+    "https://indexer.havendao.community/api/coinflip-house.near?api_key=f743dcb217d1d615dd1bf652&limit=7";
   const MAX = 5;
 
   const [active, setActive] = useState(false);
+
   const [rangeval, setRangeval] = useState(null);
   const [value, setValue] = useState(0);
+  const [user, setUser] = React.useState(null);
 
-  const getBackgroundSize = () => {
-    return { backgroundSize: `${(value * 100) / MAX}% 100%` };
-  };
+  React.useEffect(() => {
+    fetch('https://randomuser.me/api/')
+      .then(results => results.json())
+      .then(data => {
+        setUser(data.results[0]);
+        console.log(user, "testing")
+      });
+  }, []); // Pass empty array to only run once on mount.
+
+
+  
 
   const handleChangeActive = () => {
     console.log();
@@ -68,7 +58,6 @@ export default function App() {
   };
 
   return (
-    // use React Fragment, <>, to avoid wrapping elements in unnecessary divs
     <>
       <div>
         <Header />
@@ -106,92 +95,10 @@ export default function App() {
           <div className="row justify-content-center mt-4">
             <button class="button-flip">Flip 0.2 Ⓝ!</button>
           </div>
-          <div className="row justify-content-center mt-5">
-            <div className="col justify-content-center mt-4 recent-flips-card">
-              <h1 class="recent-flips">Recent Flips</h1>
-              <div className="row justify-content-center mt-4">
-                <img class="realtime-image" src={RealTimeIcon} />
-                <p class="realtime-info">
-                  swiftyyy.near flipped <span class="near-amount">0.5 Ⓝ</span>{" "}
-                  and lost
-                </p>
-                <span class="realtime-info">1m</span>
-              </div>
-              <div className="row justify-content-center mt-4">
-                <img class="realtime-image" src={RealTimeIcon} />
-                <p class="realtime-info">
-                  swiftyyy.near flipped <span class="near-amount">0.5 Ⓝ</span>{" "}
-                  and lost
-                </p>
-                <span class="realtime-info">1m</span>
-              </div>
-              <div className="row justify-content-center mt-4">
-                <img class="realtime-image" src={RealTimeIcon} />
-                <p class="realtime-info">
-                  swiftyyy.near flipped <span class="near-amount">0.5 Ⓝ</span>{" "}
-                  and lost
-                </p>
-                <span class="realtime-info">1m</span>
-              </div>
-              <div className="row justify-content-center mt-4">
-                <img class="realtime-image" src={RealTimeIcon} />
-                <p class="realtime-info">
-                  swiftyyy.near flipped <span class="near-amount">0.5 Ⓝ</span>{" "}
-                  and lost
-                </p>
-                <span class="realtime-info">1m</span>
-              </div>
-              <div className="row justify-content-center mt-4">
-                <img class="realtime-image" src={RealTimeIcon} />
-                <p class="realtime-info">
-                  swiftyyy.near flipped <span class="near-amount">0.5 Ⓝ</span>{" "}
-                  and lost
-                </p>
-                <span class="realtime-info">1m</span>
-              </div>
-              <div className="row justify-content-center mt-4">
-                <img class="realtime-image" src={RealTimeIcon} />
-                <p class="realtime-info">
-                  swiftyyy.near flipped <span class="near-amount">0.5 Ⓝ</span>{" "}
-                  and lost
-                </p>
-                <span class="realtime-info">1m</span>
-              </div>
-              <div className="row justify-content-center mt-4">
-                <img class="realtime-image" src={RealTimeIcon} />
-                <p class="realtime-info">
-                  swiftyyy.near flipped <span class="near-amount">0.5 Ⓝ</span>{" "}
-                  and lost
-                </p>
-                <span class="realtime-info">1m</span>
-              </div>
-            </div>
-          </div>
+        <FlipData/>
         </div>
         <Footer />
       </div>
     </>
   );
 }
-
-// // this component gets rendered by App after the form is submitted
-// function Notification() {
-//   const urlPrefix = `https://explorer.${networkId}.near.org/accounts`
-//   return (
-//     <aside>
-//       <a target="_blank" rel="noreferrer" href={`${urlPrefix}/${window.accountId}`}>
-//         {window.accountId}
-//       </a>
-//       {' '/* React trims whitespace around tags; insert literal space character when needed */}
-//       called method: 'set_greeting' in contract:
-//       {' '}
-//       <a target="_blank" rel="noreferrer" href={`${urlPrefix}/${window.contract.contractId}`}>
-//         {window.contract.contractId}
-//       </a>
-//       <footer>
-//         <div>✔ Succeeded</div>
-//         <div>Just now</div>
-//       </footer>
-//     </aside>
-//   )
-// }
