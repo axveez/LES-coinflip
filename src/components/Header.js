6b379df8305d42fec64b68eba40a01612aead1c9
Tommy <tommymaher10@gmail.com>
2022-03-21 19:25:59 +0000

    mobile connect wallet button done

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,14 +9,15 @@ const Header = () => {
   return (
     <div className='header'>
       <div className='header_background'></div>
-      <Stack direction='horizontal' className="header_navbar" gap={3}>
-        <Image src={KangarooLogo} /><div></div>
+      <Stack direction='horizontal' className="header_navbar header-mobile" gap={3}>
+        <Image src={KangarooLogo}  className = "logo-header"/><div></div>
         <Stack direction="horizontal" className="pull-right" gap={2}>
           <a href="/#">About</a>
           <a href="/#">How To Play</a>
           <a href="/#">FAQ</a>
-          <button className="bold-font btn-transparent header_connect_btn" onClick={()=>login()}>Connect Wallet</button>
+
         </Stack>
+        <button className="bold-font btn-transparent header_connect_btn" onClick={()=>login()}>Connect Wallet</button>
       </Stack>      
     </div>
   );
