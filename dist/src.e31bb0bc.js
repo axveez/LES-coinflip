425828620efd264ec33211d6f20d32656490ee37
Axveez <axveez@gmail.com>
2022-04-10 19:49:39 +0700

    update api

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -96198,7 +96198,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62534" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50967" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
