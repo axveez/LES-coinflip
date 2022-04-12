49109c14d46ed942616aa730bd6154eb5c109f7a
Axveez <axveez@gmail.com>
2022-04-12 10:34:48 +0700

    update header
    update header

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -93554,7 +93554,7 @@ const Home = () => {
     className: "col-12 text-center"
   }, /*#__PURE__*/_react.default.createElement("h3", {
     className: "bold-font"
-  }, "KANGAROO FLIP COIN LEADERBOARD"), /*#__PURE__*/_react.default.createElement("br", null), /*#__PURE__*/_react.default.createElement("a", {
+  }, "KANGAROO COIN FLIP LEADERBOARD"), /*#__PURE__*/_react.default.createElement("br", null), /*#__PURE__*/_react.default.createElement("a", {
     href: "/",
     className: "btn btn-leaderboard active"
   }, "Play Flip Coin"), /*#__PURE__*/_react.default.createElement("br", null), /*#__PURE__*/_react.default.createElement("br", null)), /*#__PURE__*/_react.default.createElement("div", {
@@ -96198,7 +96198,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50967" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57759" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
